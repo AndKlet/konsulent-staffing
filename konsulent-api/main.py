bf252b7aebb9cd24307e755cfd3230638f1101f4
Andreas Kletvang <andreaskletvang@outlook.com>
2025-12-17 23:20:08 +0100

    Update file read to use getcwd()

--- konsulent-api/main.py
+++ konsulent-api/main.py
@@ -6,8 +6,7 @@ app = FastAPI(title="Konsulent API", version="1.0.0")
 
 def load_data():
     """Loads consultant data from a JSON file"""
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    data_path = os.path.join(current_dir, "data.json")
+    data_path = os.path.join(os.getcwd(), "data.json")
     try:
         with open(data_path, "r", encoding="utf-8") as file:
             data = json.load(file)
